a16b70e19a9dc692dce77c3d49205163d59cdf9c
Kim in sool <pdg300@naver.com>
2022-02-08 23:43:37 +0900

    프로젝트 세팅환경 설명을 위한 readme작성과 app.js에 기본 react퍼블 제거  #002

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      React로 만든 to-do-app-list 토이 프로젝트입니다.<br/>
+      개발 기획 중 입니다...<br/>
+      # 2022-02-08 김인술<br/>
     </div>
   );
 }
